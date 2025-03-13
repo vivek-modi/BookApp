17c212b8ce8de108a55c0615fce9ae018cb4701f
vivekmodi <vivekmodi456@gmail.com>
2025-03-13 15:29:27 +0000

    Feat: Add LoadingOverlay and initial theme setup
    This commit introduces a `LoadingOverlay` composable and sets up the initial theme for the app.
    - Created `LoadingOverlay` composable to display a loading indicator with a background overlay.
    - Created `Color.kt` to define light and dark color schemes.
    - Created `Spacing.kt` to define spacing values and a spacing theme.
    - Created `Theme.kt` to implement theming using light, dark, and dynamic color schemes.
    - Created `Type.kt` to define an app-specific typography theme.

---
+++ presentation/src/main/kotlin/com/tapdoo/presentation/theme/Spacing.kt
@@ -0,0 +1,16 @@
+package com.tapdoo.presentation.theme
+
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.ui.unit.dp
+
+object Spacing {
+    val extraSmall = 4.dp
+    val small = 8.dp
+    val extraMedium = 12.dp
+    val medium = 16.dp
+    val large = 24.dp
+    val extraLarge = 32.dp
+}
+
+val MaterialTheme.spacing: Spacing
+    get() = Spacing
---
+++ presentation/src/main/kotlin/com/tapdoo/presentation/theme/Type.kt
@@ -0,0 +1,5 @@
+package com.tapdoo.presentation.theme
+
+import androidx.compose.material3.Typography
+
+val AppTypography = Typography()
